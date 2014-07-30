3af44060e5fd57bd9295e17bf01cbda03a241e3e
joseorsini <jose@dotcms.com>
2014-07-30 11:46:46 -0400

    initial commit

--- README.md
+++ README.md
@@ -6,4 +6,46 @@ This OSGi viewtool allows any dotcms user with proper permissions to run SQL sta
 Requirements:
 
 1. The User must have the "Scripting Developer" role assigned to it in order to execute this ViewTool from a Content/Widget.
-2. In case the ViewTool is intended to query a different database than dotcms, this Datasource must be defined in {dotcms_home}/tomcat/conf/Catalina/localhost/ROOT.xml file (for 2.x versions of dotcms) or in {dotcms_home}/extra/tomcat-7/context.xml (for 3.x versions of dotcms). For non-standard versions of dotcms, a different connection pool must be set directly in the Application Server.
+
+2. Scripting must be enabled in the dotcms environment. In order to enable this, this variable must be set to true in the dotmarketing-config.properties file
+
+ENABLE_SCRIPTING 
+
+This variable should be customized through a configuration plugin (static plugin).
+
+3. In case the ViewTool is intended to query a different database than dotcms, this Datasource must be defined in {dotcms_home}/tomcat/conf/Catalina/localhost/ROOT.xml file (for 2.x versions of dotcms) or in {dotcms_home}/extra/tomcat-7/context.xml (for 3.x versions of dotcms). For non-standard versions of dotcms, a different connection pool must be set directly in the Application Server.
+
+4. If the user wants to use the ViewTool for querying the dotcms database:
+
+a) It can create a different datasource with limited permissions over the database/schema (following the 2nd Requirement from this document).
+
+b) If it's not possible to create a limited datasource, it can query the dotcms database with the "default" datasource as long as this variable is set to true in the dotmarketing-config.properties file
+
+ALLOW_VELOCITY_SQL_ACCESS_TO_DOTCMS_DB
+
+This variable should be customized through a configuration plugin (static plugin).
+
+How to use it: 
+
+#set($resultsList = $sqlResultsTool.getSQLResults(String datasource, String query, int startRow, int maxRow))
+
+a) "datasource": can be set to:
+
+- "default": The default datasource specified for accessing the dotcms database.
+- "custom": A custom datasource specified for limited access to dotcmcs database or any other database.
+
+b) "query": The SQL query you want to run from a Content/Widget in a frontend page. Restricted queries are:
+
+- DELETE FROM.
+- DROP.
+- TRUNCATE.
+- ALTER DATABASE.
+- ALTER TABLE.
+- Any query that hits the "user_" or "cms_role" tables from the dotcms database.
+- All queries or operations set at database level by the DBA.
+
+c) "startRow": It allows results pagination. If pagination is not desired, set it to 0.
+
+d) "maxRow": Sets a limit of results to display from the query's output.
+
+
---
+++ src/main/java/com/dotmarketing/osgi/viewtools/SQLResultsViewTool.java
@@ -0,0 +1,172 @@
+package com.dotmarketing.osgi.viewtools;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.velocity.context.Context;
+import org.apache.velocity.context.InternalContextAdapterImpl;
+import org.apache.velocity.tools.view.context.ViewContext;
+import org.apache.velocity.tools.view.tools.ViewTool;
+
+import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.Role;
+import com.dotmarketing.business.UserAPI;
+import com.dotmarketing.common.db.DotConnect;
+import com.dotmarketing.exception.DotDataException;
+
+import com.dotmarketing.portlets.contentlet.model.Contentlet;
+import com.dotmarketing.util.Config;
+import com.dotmarketing.util.Logger;
+import com.dotmarketing.util.UtilMethods;
+import com.liferay.portal.model.User;
+
+public class SQLResultsViewTool implements ViewTool {
+
+	private static final UserAPI userAPI = APILocator.getUserAPI();
+    Context ctx;
+	private InternalContextAdapterImpl ica;
+
+	public void init(Object obj) {
+
+        ViewContext context = (ViewContext) obj;
+		ctx = context.getVelocityContext();
+	
+	}
+
+    public ArrayList<HashMap<String, String>> getSQLResults(String dataSource, String sql, int startRow, int maxRow) {
+		   
+		   ArrayList<HashMap<String, String>> errorResults = new ArrayList<HashMap<String, String>>();
+		   
+		   if(!canUserEvaluate()){
+	            HashMap<String, String> map = new HashMap<String, String>();
+	            map.put("hasDotConnectSQLError", "true");
+	            map.put("dotConnectSQLError", "External scripting is disabled in your dotcms instance.");
+	            errorResults.add(map);
+	            return errorResults;
+		   }
+		   else{
+			   
+			   try{
+				   
+				   ica = new InternalContextAdapterImpl(ctx);
+	               String fieldResourceName = ica.getCurrentTemplateName();
+	               String conInode = fieldResourceName.substring(fieldResourceName.indexOf("/") + 1, fieldResourceName.indexOf("_"));
+
+	               Contentlet con = APILocator.getContentletAPI().find(conInode, APILocator.getUserAPI().getSystemUser(), true);
+
+				   if (!UtilMethods.isSet(sql)) {
+			            return new ArrayList<HashMap<String, String>>();
+			        }
+			        if (sql.toLowerCase().indexOf("user_") > -1) {
+			            Logger.error(this,"getSQLResults macro is trying to query the user_ table");
+	                    Logger.debug(this,"Check content with id: " + con.getIdentifier());
+			            HashMap<String, String> map = new HashMap<String, String>();
+			            map.put("hasDotConnectSQLError", "true");
+			            map.put("dotConnectSQLError", "getSQLResults macro is trying to query the user_ table");
+			            errorResults.add(map);
+			            return new ArrayList<HashMap<String, String>>();
+			        }
+			        if (sql.toLowerCase().indexOf("cms_role") > -1) {
+			            Logger.error(this,"getSQLResults macro is trying to query the cms_role table");
+	                    Logger.debug(this,"Check content with id: " + con.getIdentifier());
+			            HashMap<String, String> map = new HashMap<String, String>();
+			            map.put("hasDotConnectSQLError", "true");
+			            map.put("dotConnectSQLError", "getSQLResults macro is trying to query the cms_role table");
+			            errorResults.add(map);
+			            return new ArrayList<HashMap<String, String>>();
+			        }
+			        if (sql.toLowerCase().indexOf("delete from") > -1 || sql.toLowerCase().indexOf("drop") > -1
+			        		|| sql.toLowerCase().indexOf("truncate") > -1 || sql.toLowerCase().indexOf("alter table") > -1
+	                        || sql.toLowerCase().indexOf("alter database") > -1 || sql.toLowerCase().startsWith("update")) {
+			            Logger.error(this,"getSQLResults macro is trying to run a forbidden query");
+	                    Logger.debug(this,"Check content with id: " + con.getIdentifier());
+			            HashMap<String, String> map = new HashMap<String, String>();
+			            map.put("hasDotConnectSQLError", "true");
+			            map.put("dotConnectSQLError", "getSQLResults macro is trying to run a forbidden query");
+			            errorResults.add(map);
+			            return new ArrayList<HashMap<String, String>>();
+			        }
+			        
+			        try {
+			            DotConnect dc = new DotConnect();
+
+			            dc.setSQL(sql);
+
+	                    if(!UtilMethods.isSet(startRow)){
+	                        startRow = 0;
+	                    }else{
+	    		            if (startRow > 0) {
+	    		                dc.setStartRow(startRow);
+	    		            }
+	                    }
+
+	                    if(!UtilMethods.isSet(maxRow)){
+	                        maxRow = 0;
+	                    }else{
+	    		            if (maxRow > 0) {
+	    		                dc.setMaxRows(maxRow);
+	    		            }
+	                    }
+
+			            if (dataSource.equals("default")) {
+			            	if(!Config.getBooleanProperty("ALLOW_VELOCITY_SQL_ACCESS_TO_DOTCMS_DB", false)){
+			            		Logger.error(this,"getSQLResults macro is trying to execute queries using the default connection pool.");
+			            		Logger.debug(this,"ALLOW_VELOCITY_SQL_ACCESS_TO_DOTCMS_DB is set to false");
+					            HashMap<String, String> map = new HashMap<String, String>();
+					            map.put("hasDotConnectSQLError", "true");
+					            map.put("dotConnectSQLError", "getSQLResults macro is trying to execute queries using the default connection pool. ALLOW_VELOCITY_SQL_ACCESS_TO_DOTCMS_DB is set to false");
+					            errorResults.add(map);
+					            return new ArrayList<HashMap<String, String>>();
+			            	}
+			            	else{
+			            		return dc.getResults();
+			            	}
+			            } else {
+			                return dc.getResults(dataSource);
+			            }
+			        } catch (Exception e) {
+			            HashMap<String, String> map = new HashMap<String, String>();
+			            map.put("hasDotConnectSQLError", "true");
+			            map.put("dotConnectSQLError", "There was a sql error:" + e.getMessage());
+			            errorResults.add(map);
+			            return errorResults;
+			        }
+				   
+			   }
+			   catch(Exception e){
+				   Logger.error(this,"There was a problem retrieving the content where ViewTool was called");
+		           HashMap<String, String> map = new HashMap<String, String>();
+		           map.put("hasDotConnectSQLError", "true");
+		           map.put("dotConnectSQLError", "There was a sql error:" + e.getMessage());
+		           errorResults.add(map);
+		           return errorResults;
+
+                
+			   }
+		   }
+		   
+	    }
+
+	    protected boolean canUserEvaluate(){
+		    if(!Config.getBooleanProperty("ENABLE_SCRIPTING", false)){
+			    Logger.warn(this.getClass(), "Scripting called and ENABLE_SCRIPTING set to false");
+			    return false;
+		    }
+            try{
+                ica = new InternalContextAdapterImpl(ctx);
+                String fieldResourceName = ica.getCurrentTemplateName();
+                String conInode = fieldResourceName.substring(fieldResourceName.indexOf("/") + 1, fieldResourceName.indexOf("_"));
+
+                Contentlet con = APILocator.getContentletAPI().find(conInode, APILocator.getUserAPI().getSystemUser(), true);
+
+                User mu = userAPI.loadUserById(con.getModUser(), APILocator.getUserAPI().getSystemUser(), true);
+                Role scripting =APILocator.getRoleAPI().loadRoleByKey("Scripting Developer");
+                return APILocator.getRoleAPI().doesUserHaveRole(mu, scripting);
+		    }
+		    catch(Exception e){
+			    Logger.warn(this.getClass(), "Scripting called with error" + e);
+			    return false;	
+		    }
+	    }
+
+}
